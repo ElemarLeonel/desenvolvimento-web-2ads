2b980d653fbc427493c58ebbdf3ddd8124588b0e
ElemarLeonel <elemarleonelbalduino@gmail.com>
2025-11-05 14:15:26 -0400

    Adicionado comentários sobre o assunto

--- script.js
+++ script.js
@@ -15,18 +15,24 @@ document.addEventListener("load", function(){
     alert("Carreguei");
 })
 
+// Evento click
 botaoNome.addEventListener("click", function(){
 //    alert(`Cheguei nesse botão! Eu sou ${nome}`);
     nomes.forEach(nome => {
         alert(`Eu sou ${nome}`);
     })
 
+    // Concatenando os nomes e separando por vírgula
     const resultado = nomes.join(", ");
 
+    // Validação pelo nome "João" dentro do array
     if(nomes.includes("João")){
+        // Alterando a cor de fundo do parágrafo para azul
         paragrafo2.style.backgroundColor = "blue";
+        // Alterando a cor do texto do parágrafo para branco
         paragrafo2.style.color = "white";
     }
 
+    // Escrevendo o texto dentro do parágrafo
     paragrafo2.textContent = resultado;
 })
